79b2574c2f38ef4534401e17e1210150d14f7b3c
bdiaz12345 <bdiazmusic5@gmail.com>
2021-01-15 12:14:36 -0500

    mvp completed

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,22 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+import * as rtl from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from '@testing-library/user-event';
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    rtl.render(<CheckoutForm />)
+    const header = rtl.screen.queryByText(/checkout form/i)
+    expect(header).toBeInTheDocument()
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    rtl.render(<CheckoutForm />)
+    const button = document.querySelector('button')
+    expect(button).toBeInTheDocument()
+    userEvent.click(button)
+    const successMessage = document.querySelector('.success-message')
+    expect(successMessage).toBeInTheDocument()
+});
